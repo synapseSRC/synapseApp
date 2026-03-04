b8599543925af44ee2a24bb758cbcc0371a78764
Ashik Ahmed <177647015+TheRealAshik@users.noreply.github.com>
2026-03-04 22:48:26 +0600

    Delete 20250101000000_increment_post_reshares.sql

--- supabase/migrations/20250101000000_increment_post_reshares.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION increment_post_reshares(post_id UUID)
-RETURNS void
-LANGUAGE sql
-AS $$
-  UPDATE posts
-  SET reshares_count = reshares_count + 1
-  WHERE id = post_id;
-$$;
