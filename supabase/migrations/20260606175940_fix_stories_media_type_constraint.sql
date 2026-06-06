05bd49a9b1e8b462d6b6e350b02347cb7a67cda3
TheRealAshik <mashikahamed0@gmail.com>
2026-06-06 18:18:15 +0000

    chore: remove migrations folder, setup.sql is source of truth

--- supabase/migrations/20260606175940_fix_stories_media_type_constraint.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE public.stories
-  DROP CONSTRAINT stories_media_type_check,
-  ADD CONSTRAINT stories_media_type_check
-    CHECK (media_type = ANY (ARRAY['image'::text, 'video'::text, 'text'::text, 'photo'::text]));
